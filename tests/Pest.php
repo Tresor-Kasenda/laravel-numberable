7eb7f57645accbee37c3496dd4e7a4e0e9f1c7af
Tresor kasenda <tresorkasendat@gmail.com>
2026-02-23 17:04:34 +0200

    Make Pest bootstrap compatible with Pest 2

--- tests/Pest.php
+++ tests/Pest.php
@@ -11,7 +11,7 @@
 |
 */
 
-pest()->extend(TresorKasenda\Numberable\Tests\TestCase::class)
+uses(TresorKasenda\Numberable\Tests\TestCase::class)
  // ->use(Illuminate\Foundation\Testing\RefreshDatabase::class)
     ->in('Feature');
 
