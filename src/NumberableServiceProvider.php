c536673af4acd4eed513c56afebaf9731fdf6f6e
Tresor kasenda <tresorkasendat@gmail.com>
2026-03-01 06:54:35 +0200

    Add support for arbitrary-precision decimal operations with Brick Math

--- src/NumberableServiceProvider.php
+++ src/NumberableServiceProvider.php
@@ -8,12 +8,12 @@ class NumberableServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        // Rien à binder, la classe est stateless + fluent
+        //
     }
 
     public function boot(): void
     {
-        // Optionnel : publier une config future
+        // Optionnel
         // $this->publishes([
         //     __DIR__.'/../config/numberable.php' => config_path('numberable.php'),
         // ], 'numberable-config');
